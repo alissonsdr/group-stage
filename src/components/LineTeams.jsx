8787b7505f68bf6181a4e5025a45f837747ebe32
alissonsdr <alisson_sr@outlook.com>
2024-04-19 19:02:29 -0300

    design

---
+++ src/components/LineTeams.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './LineTeams.css'
+
+const LineTeams = () => {
+
+    const line = ''
+
+  return (
+    <div className='lineTeam'>
+        {line.map((team) => {
+            
+        })}
+        <span>21/nov</span>
+        <span>time1 <input type="number"/> x <input type="number" /> time2</span>
+        <span>resultado</span>
+    </div>
+  )
+}
+
+export default LineTeams
